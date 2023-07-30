09c044d66ada715afe0e7bffb4dee2df0dacd05c
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2023-07-29 23:13:31 -0700

    Adding hyperboloid

--- Hyperbolic/PseudoEuclideanSpace.lean
+++ Hyperbolic/PseudoEuclideanSpace.lean
@@ -1,4 +1,6 @@
 import Mathlib.Analysis.InnerProductSpace.Basic
+import Mathlib.Topology.MetricSpace.Basic
+import Hyperbolic.Arcosh
 
 open BigOperators
 
@@ -13,7 +15,7 @@ def MinusSign : Sign := ⟨-1, by right; rfl⟩
 end PseudoEuclideanSpace
 
 @[reducible, nolint unusedArguments]
-def PseudoEuclideanSpace {f : Type _} [Fintype f] (signature: f → PseudoEuclideanSpace.Sign) := f → ℝ
+def PseudoEuclideanSpace {f : Type _} [Fintype f] (_: f → PseudoEuclideanSpace.Sign) := f → ℝ
 
 instance : AddCommGroup (@PseudoEuclideanSpace f k signature) := inferInstanceAs <| AddCommGroup (f → ℝ)
 noncomputable instance : Module ℝ (@PseudoEuclideanSpace f k signature) := by delta PseudoEuclideanSpace; infer_instance
@@ -24,4 +26,20 @@ instance : Inner ℝ (@PseudoEuclideanSpace f k signature) :=
 def MinkowskiSpaceSignature (n : ℕ) : Fin n → PseudoEuclideanSpace.Sign :=
     fun i => if (↑i : ℕ) == 0 then PseudoEuclideanSpace.MinusSign else PseudoEuclideanSpace.PlusSign
 
-def MinkowskiSpace (d : ℕ) := PseudoEuclideanSpace (MinkowskiSpaceSignature d)
+def MinkowskiSpace (d : ℕ+) := PseudoEuclideanSpace (MinkowskiSpaceSignature d)
+
+instance : Inner ℝ (MinkowskiSpace d) := by delta MinkowskiSpace; infer_instance
+
+def TimeLike (v : MinkowskiSpace d) : Prop := ⟪v,v⟫_ℝ < 0
+def UnitTimeLike (v : MinkowskiSpace d) : Prop := ⟪v,v⟫_ℝ = -1
+def LightLike (v : MinkowskiSpace d) : Prop := ⟪v,v⟫_ℝ = 0
+def SpaceLike (v : MinkowskiSpace d) : Prop := ⟪v,v⟫_ℝ > 0
+def UnitSpaceLike (v : MinkowskiSpace d) : Prop := ⟪v,v⟫_ℝ =1
+
+def Future (v : MinkowskiSpace d) : Prop := v 0 > 0
+
+def Hyperboloid (d : ℕ+) := { p : MinkowskiSpace d // TimeLike p ∧ Future p}
+
+noncomputable instance : Dist (MinkowskiSpace d) :=
+  ⟨fun v w => Real.arcosh (-(⟪v, w⟫_ℝ))⟩
+
